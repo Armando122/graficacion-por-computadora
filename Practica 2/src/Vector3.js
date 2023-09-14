4b8c8ea645fe50ec39aa322d5485069dfa713db5
Armando Ramírez <armandoramirezg@ciencias.unam.mx>
2023-09-14 01:12:54 -0600

    Iniciando clase Vector3

--- Practica 2/src/Vector3.js
+++ Practica 2/src/Vector3.js
@@ -1,6 +1,36 @@
 var CG = (function(CG) {
     class Vector3 {
-        //Codigo
+        /**
+         * @param x
+         * @param y
+         * @param z
+         * En caso de no recibir valores devuelve (0,0,0)
+         */
+        constructor(x,y,z) {
+            if (x == null && y == null && z == null) {
+                this.x = 0;
+                this.y = 0;
+                this.z = 0;
+            } else {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+
+        /**
+         * @param {Vector3} u
+         * @param {Vector3} v
+         * @return {Vector3}
+         * Devuelve la suma de los vectores
+         */
+        static add(u, v) {
+            let nx = u.x + v.x;
+            let ny = u.y + v.y;
+            let nz = u.z + v.z;
+            let vect = new Vector3(nx,ny,nz);
+            return vect;
+        }
     }
 
     CG.Vector3 = Vector3;
