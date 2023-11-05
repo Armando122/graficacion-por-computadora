dbc980bc8bdfa31839a83070fa9736726d8cd61e
Armando Ramírez <armandoramirezg@ciencias.unam.mx>
2023-11-05 14:09:11 -0600

    Coordenadas UV para icosaedro completas

--- Practica_6/src/Main.js
+++ Practica_6/src/Main.js
@@ -38,12 +38,12 @@ window.addEventListener("load", async function(evt) {
         [0, 1, 1, 1],
         2, 16, 16, 
         CG.Matrix4.translate(new CG.Vector3(-5, 0, 0))
-      ),
+      ),*/
       new CG.Icosaedro(gl, 
         [1, 0 , 1, 1], 
         2, 
         CG.Matrix4.translate(new CG.Vector3(0, 0, 0))
-      ),*/
+      ),
       new CG.Octaedro(
         gl, 
         [1, 1, 0, 1], 
@@ -75,10 +75,10 @@ window.addEventListener("load", async function(evt) {
     // Cono 
     geometry[0].setTexture(await CG.loadImage("texturas/Tiles074_1K-PNG_Color.png")); // Dodecaedro 
     // Esfera 
-    // Icosaedro 
-    geometry[1].setTexture(await CG.loadImage("texturas/Tiles093_2K-PNG_Color.png")); // Octaedro 
-    geometry[2].setTexture(await CG.loadImage("texturas/Bricks086_1K-PNG_Color.png")); // Prisma rectangular 
-    geometry[3].setTexture(await CG.loadImage("texturas/WoodFloor051_2K-PNG_Color.png")); // Tetraedro 
+    geometry[1].setTexture(await CG.loadImage("texturas/Gravel023_2K-PNG_AmbientOcclusion.png")); // Icosaedro 
+    geometry[2].setTexture(await CG.loadImage("texturas/Tiles093_2K-PNG_Color.png")); // Octaedro 
+    geometry[3].setTexture(await CG.loadImage("texturas/Bricks086_1K-PNG_Color.png")); // Prisma rectangular 
+    geometry[4].setTexture(await CG.loadImage("texturas/WoodFloor051_2K-PNG_Color.png")); // Tetraedro 
     // Toro 
 
     // se determina el color con el que se limpia la pantalla, en este caso un color negro transparente
